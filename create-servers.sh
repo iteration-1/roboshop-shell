49baddd2d8a511d22710432db1543a04afd420d1
pappik <pappik@gmail.com>
2024-03-22 22:11:46 -0400

    create servers

--- create-servers.sh
+++ create-servers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##### Change these values ###
-ZONE_ID="Z0366464237Z7LZLZPKFA"
-DOMAIN="devopsb70.online"
+ZONE_ID="Z05654942XW5Z9U75CZF9"
+DOMAIN="pappik.online"
 SG_NAME="allow-all"
 env=dev
 #############################
