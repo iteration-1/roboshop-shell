05fe6ab60bd042572e751ea9fd3c9d00614f1a92
pappik <pappik@gmail.com>
2024-03-23 21:08:36 -0400

    redis

--- mysql.sh
+++ mysql.sh
@@ -1,11 +1,15 @@
 source common.sh
 
 print_head "install mysql"
-dnf install mysql-community-server -y &>>${LOG}
+dnf install mysql -y &>>${LOG}
 status_check
 
 print_head "setup mysql passwd"
 mysql_secure_installation --set-root-pass RoboShop@1 &>>${LOG}
 status_check
 
-systemd
+print_head "enable mysqld"
+systemctl enable mysqld
+
+print_head "restart mysqld"
+systemctl restart mysqld
